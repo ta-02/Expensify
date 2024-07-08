1bd2b8662420900878cb60f2ad3c79c12193e7ef
ta-02 <talal.gl2004@gmail.com>
2024-07-08 00:04:44 -0400

    added some trpc stuff

--- server/app.ts
+++ server/app.ts
@@ -3,12 +3,16 @@ import morgan from "morgan";
 import bodyParser from "body-parser";
 import router from "./router";
 import path from "path";
+import { createExpressMiddleware } from "@trpc/server/adapters/express";
+import { appRouter } from "./trpc";
 
 const app: Express = express();
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 
-app.use("/api", router);
+app.use("/trpc", createExpressMiddleware({ router: appRouter }));
+
+const apiRoutes = app.use("/api", router);
 
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
