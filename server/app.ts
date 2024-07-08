def27de4eaef9dcf80a3c22be697fdc31b6a92f5
ta-02 <talal.gl2004@gmail.com>
2024-07-08 00:15:58 -0400

    removing trpc

--- server/app.ts
+++ server/app.ts
@@ -3,14 +3,10 @@ import morgan from "morgan";
 import bodyParser from "body-parser";
 import router from "./router";
 import path from "path";
-import { createExpressMiddleware } from "@trpc/server/adapters/express";
-import { appRouter } from "./trpc";
 
 const app: Express = express();
 app.use(morgan("dev"));
 app.use(bodyParser.json());
-
-app.use("/trpc", createExpressMiddleware({ router: appRouter }));
 
 const apiRoutes = app.use("/api", router);
 
